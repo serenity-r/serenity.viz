6ea46fa0c1a99daca9feadea6bfc2260d9881412
M. Drew LaMar <mdlama@wm.edu>
2020-01-10 10:13:09 -0500

    [feat] Finished rug params module

--- R/module_layer_params_rug.R
+++ R/module_layer_params_rug.R
@@ -6,38 +6,54 @@ layerParamsGeomRugUI <- function(id) {
 
 layerParamsGeomRugServer <- function(input, output, session, base_data) {
   # Reactive since sides defaults changes based on data
-  default_args <- reactiveValues("sides" = c("b", "l"))
+  default_args <- reactiveValues("sides" = c("b", "l"),
+                                 "length" = unit(0.03, 'npc'),
+                                 "outside" = FALSE)
 
   # Need to hijack sides input as shinyWidgets returns NULL instead
   #   of character(0) when nothing is selected
-  layer_data <- reactiveValues(sides = character(0))
+  layer_data <- reactiveValues(sides = character(0),
+                               length = unit(0.03, 'npc')) # Should set to default in observeEvent - being lazy here
 
   reactive_inputs <- reactive({
     paste(
       layer_data$sides,
-      default_args$sides
+      default_args$sides,
+      layer_data$length,
+      input$outside
     )
   })
 
   output$params <- renderUI({
     if (isTruthy(base_data())) {
       isolate({
-        shinyWidgets::checkboxGroupButtons(
-          session$ns('sides_original'),
-          label = "Which sides?",
-          choices = c(switch(!is.null(base_data()$x),
-                             c("Bottom" = "b",
-                               "Top" = "t")),
-                      switch(!is.null(base_data()$y),
-                             c("Left" = "l",
-                               "Right" = "r"))
+        tagList(
+          shinyWidgets::checkboxGroupButtons(
+            session$ns('sides_original'),
+            label = "Which sides?",
+            choices = c(switch(!is.null(base_data()$x),
+                               c("Bottom" = "b",
+                                 "Top" = "t")),
+                        switch(!is.null(base_data()$y),
+                               c("Left" = "l",
+                                 "Right" = "r"))
+            ),
+            selected = input[['sides_original']] %||% default_args[['sides']],
+            status = "default",
+            size = "xs",
+            checkIcon = list(
+              yes = icon("ok", lib = "glyphicon"),
+              no = icon("remove", lib = "glyphicon")
+            )
           ),
-          selected = input[['sides_original']] %||% default_args[['sides']],
-          status = "default",
-          size = "xs",
-          checkIcon = list(
-            yes = icon("ok", lib = "glyphicon"),
-            no = icon("remove", lib = "glyphicon")
+          unitChooserUI(session$ns('length'), "Rug length"),
+          div(
+            class = "inline-switch-with-label",
+            h5("Outside plot"),
+            switchInput(session$ns('outside'),
+                        value = input[['outside']] %||% default_args[['outside']],
+                        size = "mini",
+                        inline = TRUE)
           )
         )
       })
@@ -46,6 +62,13 @@ layerParamsGeomRugServer <- function(input, output, session, base_data) {
 
   # _ Make sure params always update ====
   outputOptions(output, "params", suspendWhenHidden = FALSE)
+
+  rug_length <- callModule(unitChooserServer, "length")
+
+  # Update length input
+  observeEvent(rug_length(), {
+    layer_data$length <- rug_length()
+  })
 
   # Update sides input (NULL -> character(0))
   observeEvent(input$sides_original, {
@@ -82,11 +105,21 @@ layerParamsGeomRugServer <- function(input, output, session, base_data) {
       processed_geom_params_code <- process_args(reactiveValuesToList(default_args),
                                                  c(reactiveValuesToList(input),
                                                    reactiveValuesToList(layer_data)),
-                                                 NULL)
+                                                 NULL,
+                                                 modify_geom_rug_args)
     })
 
     return(processed_geom_params_code)
   })
 
   return(geom_params_code)
+}
+
+modify_geom_rug_args <- function(param, value, base_data) {
+  return(
+    switch(param,
+           "length" = paste0("unit(", as.numeric(value), ", '", attr(value, "unit"), "')"),
+           value
+    )
+  )
 }
